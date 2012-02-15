0e26334b2e2daa1dd461fd478d948d0c3fad47b8
jamiemill <jamiermill@gmail.com>
2012-02-15 17:30:47 +0000

    Made TOC into links.

--- toc-generator.rb
+++ toc-generator.rb
@@ -1,3 +1,14 @@
+# Generates a table of contents for an html page
+# by looking at its heading tags.
+#
+# Also outputs modified HTML where the heading tags have
+# been given anchors to be targetted by the TOC links.
+#
+# WARNING: this will barf if the headings skip levels, e.g.
+# h1 -> h3 without going via h2. Fix this somehow by inserting
+# dummy intermediate headings
+
+
 require 'nokogiri'
 
 class TocGenerator
@@ -14,29 +25,30 @@ class TocGenerator
     last_entry_refs_by_depth = {
       0 => toc
     }
+    heading_counter = 1
     headings.each do |heading|
       depth = heading.name.gsub(/h/i, '').to_i
       text = heading.text
-      puts text+depth.to_s
       e = TocEntry.new
       e.name = text
-      puts last_entry_refs_by_depth[depth-1].class
+      e.counter = heading_counter
       last_entry_refs_by_depth[depth-1].children << e
       last_entry_refs_by_depth[depth] = e
+      heading_counter += 1
     end
 
     html_for_toc toc
   end
 
+  # Recusive function
+
   def html_for_toc(toc)
     out = ''
-    out << toc.name+"\n" unless toc.is_root
+    out << '<a href="#section-'+toc.counter.to_s+'">' + toc.name + "</a>\n" unless toc.is_root
     if !toc.children.empty?
       out << "<ul>\n"
       toc.children.each do |c|
-        out << "<li>\n"
-        out << html_for_toc(c)
-        out << "</li>\n"
+        out << "<li>\n" + html_for_toc(c) + "</li>\n"
       end
       out << "</ul>\n"
     end
@@ -50,7 +62,7 @@ class TocGenerator
 end
 
 class TocEntry
-  attr_accessor :name, :children, :is_root
+  attr_accessor :name, :children, :is_root, :counter
   def initialize
     @children = Array.new
   end
